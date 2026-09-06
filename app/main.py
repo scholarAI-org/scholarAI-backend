86be4ff8a426a32d1d54c2428cfd250d4af6abd8
Ahmed Zenaty <111180377+ZenatyAh@users.noreply.github.com>
2026-09-06 21:48:54 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -49,7 +49,8 @@ origins = [
     "http://127.0.0.1:5500",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://scholar-ai-neon.vercel.app/"
+    "https://scholar-ai-neon.vercel.app",
+     "https://scholar-636fa2nsy-zenatyahgmailcoms-projects.vercel.app",
 ]
 frontend_url = os.getenv("FRONTEND_URL")
 if frontend_url:
